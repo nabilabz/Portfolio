e11dc9ac256dc21ab84ed54f69e305a541a5433d
nabilabz <nabilazaini20@gmail.com>
2024-09-28 15:31:24 +0700

    perbarui porto

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+$(document).ready(function() {
+    $(".about-exp-item-detail").hide();  // Sembunyikan semua detail
+});
+
+
 function toggleDetail(e) {
-   const target = $(e.target)
+    const target = $(e.target);
+    const item = $(target).parents(".about-exp-item");
+    const detail = $(item).children(".about-exp-item-detail");
 
-   if ($(target).hasClass("active")) {
-        $(target).html("More Info").removeClass("active")
-   }else{
-        $(target).html("Less Info").addClass("active")
-   }
-
-   const item = $(target).parents(".about-exp-item")
-   const detail = $(item).children(".about-exp-item-detail")
-
-   $(detail).slideToggle()
+    $(detail).slideToggle(() => {
+        if ($(detail).is(":hidden")) {
+            $(target).html("More Info").removeClass("active");
+        } else {
+            $(target).html("Less Info").addClass("active");
+        }
+    });
 }
 
 function onFormSubmit(e) {
